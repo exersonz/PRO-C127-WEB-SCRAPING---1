313c1786faf5f61e0fb2e9d63e2fec6ff8ac6db3
exersonz <86198455+exersonz@users.noreply.github.com>
2022-08-13 11:19:55 -0700

    Add files via upload

--- web_scraping_1.py
+++ web_scraping_1.py
@@ -1,4 +1,3 @@
-from attr import attr
 from bs4 import BeautifulSoup
 import time
 import csv
@@ -13,21 +12,13 @@ def scrape():
     star_data = []
     for i in range(0, 5):
         soup = BeautifulSoup(page.content, 'html.parser')
-        for table in soup.find_all('table', attrs={'class': 'wikitable sortable jquery-tablesorter'}):
-            thead_tags = table.find_all('thead')
+        for tr_tag in soup.find_all('tr'):
+            th_tags = tr_tag.find_all('th')
             temp_list = []
-            for thead_tag in thead_tags:
-                tr_tags = thead_tag.find_all('tr')
-                try:
-                    temp_list.append(tr_tags.contents[0])
-                except:
-                    temp_list.append('')
-            for tr_tag in tr_tags:
-                th_tags = tr_tag.find_all('th')
-                try:
-                    temp_list.append(th_tags.contents[0])
-                except:
-                    temp_list.append('')
+            try:
+                temp_list.append(th_tags.contents[0])
+            except:
+                temp_list.append('')
             star_data.append(temp_list)
     with open('scrapper.csv', 'w') as m:
         csvwriter = csv.writer(m)
