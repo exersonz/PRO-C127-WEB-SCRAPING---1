90b09fab63441f30249cff7a0ca080d2de9db9fd
exersonz <86198455+exersonz@users.noreply.github.com>
2022-08-14 10:00:45 -0700

    Add files via upload

--- web_scraping_1.py
+++ web_scraping_1.py
@@ -1,28 +1,37 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import time
-import csv
 import requests
+import pandas as pd
 
 START_URL = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
 page = requests.get(START_URL)
+print(page)
 time.sleep(5)
 
-def scrape():
-    headers = ['name', 'distance', 'mass', 'radius']
-    star_data = []
-    for i in range(0, 5):
-        soup = BeautifulSoup(page.content, 'html.parser')
-        for tr_tag in soup.find_all('tr'):
-            th_tags = tr_tag.find_all('th')
-            temp_list = []
-            try:
-                temp_list.append(th_tags.contents[0])
-            except:
-                temp_list.append('')
-            star_data.append(temp_list)
-    with open('scrapper.csv', 'w') as m:
-        csvwriter = csv.writer(m)
-        csvwriter.writerow(headers)
-        csvwriter.writerows(star_data)
-    
-scrape()  
+star_data = []
+soup = bs(page.text, 'html.parser') #text is the content of the response in uni code, content is the content of the response in bytes
+star_table = soup.find('table')
+temp_list = []
+table_rows = star_table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td] #rstrip removes the spaces
+    temp_list.append(row)
+
+star_name = []
+distance = []
+mass = []
+radius = []
+lum = []
+
+for i in range(1, len(temp_list)):
+    star_name.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
+    lum.append(temp_list[i][7])
+
+    df2 = pd.DataFrame(list(zip(star_name, distance, mass, radius, lum)), columns=['star_name', 'distance', 'mass', 'radius', 'lum'])
+    print(df2)
+    df2.to_csv('bright_stars.csv')
